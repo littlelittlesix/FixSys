25b47a23733f6bc10250fb827eb3bda2a84a4ed5
1043978101@qq.com <md8817369>
2021-08-19 10:38:21 +0800

    Initial commit

---
+++ src/main/java/cn/edu/guet/FixSys/FixSysApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.FixSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixSysApplication.class, args);
+	}
+
+}
